ef9b1c326eeec30e8c632030ef170dc46033dd2b
ktmellow <letsspamkt@gmail.com>
2015-10-13 17:29:10 -0700

    authorization and authentication added

--- lilApp.py
+++ lilApp.py
@@ -1,9 +1,14 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort, make_response
 import urllib
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+from sys import argv
+
+sys, API_KEY = argv
+APP_ID = 'Y9zKGOVamfBLiBhCPvoFRKMupn3H5Trxh_4ThDd__OG4Zy7g'
 
 app = Flask(__name__)
-
-APP_ID = 'Y9zKGOVamfBLiBhCPvoFRKMupn3H5Trxh_4ThDd__OG4Zy7g'
 
 @app.route("/")
 def index():
@@ -31,12 +36,26 @@ def my_form_post():
 def next():
 	#search=request.cookies.get('search')
 	#service=request.cookies.get('service')
-	search = request.args['search']
-	service = request.args['service']
-	return render_template("nextTemplate.html", search=search, service=service)
-# use account id from callback URL	
-# https://api.kloudless.com:443/v0/accounts/86879941/search/?q=Pics
-# need to authorize first with API Key!
-	
+    search = request.args['search']
+    service = request.args['service']
+    account = request.args['account']
+    search_link = "https://api.kloudless.com:443/v0/accounts/"+account+"/search/?q="+search
+    search_results = requests.get(search_link, auth=HTTPBasicAuth(APP_ID, API_KEY))
+    results_objects = json.loads(search_results.text)
+    all_id = []
+    # print results_text 
+    # lots of junk, must further filter it 
+    for every_object in range(len(results_objects['objects'])-1):
+        # for every object in page_json, get ID 
+        all_id[every_object] = results_objects['objects'][every_object][2][id]
+        print all_id
+# YOU don't need a loop, just send a loop to template with curly braces
+
+
+    # make a link (post) for each file or folder, works for files/folders POST /accounts/{account_id}/links/ only need account and file id
+    # use requests instead of curl to handle this^ (see make a link doc in docs)
+    return render_template("nextTemplate.html", search=search, service=service, account=account)
+
+
 if __name__ == "__main__":
 	app.run(debug=True)
